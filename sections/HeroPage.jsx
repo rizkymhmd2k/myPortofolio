1c1a99167dd10b9239204576369f011bc1808fdb
Muhammad Rizky <muhammadrizky@rizkym1.local>
2023-04-27 20:49:04 +0800

    hero

--- sections/HeroPage.jsx
+++ sections/HeroPage.jsx
@@ -3,12 +3,11 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { startingFeatures } from '../constants';
-import { StartSteps, TitleText, TypingText } from '../components';
-import { fadeIn, slideIn, boxes } from '../utils/motion';
+import { TitleText, TypingText } from '../components';
+import { fadeIn, boxes } from '../utils/motion';
 
 const HeroPage = () => (
-  <section id="home" className={`${styles.paddings} 	 relative z-10 min-h-[800px]`}>
+  <section id="home" className={`${styles.paddings} relative z-10 min-h-[800px]`}>
     <div className="gradient-04 z-0" />
     <motion.div
       initial="hidden"
@@ -22,16 +21,14 @@ const HeroPage = () => (
       >
         {/* <div className={`flex relative justify-center items-center`}> */}
 
-        <div className={`inline-block relative items-center justify-center`}
-        >
+        <div className="inline-block relative items-center justify-center">
           <img
-            src="/kiki03.png"
+            src="./kiki03.png"
             alt="get-started"
             className="w-96 object-contain"
           />
 
-
-          <motion.div className='absolute top-20 left-0'
+          <motion.div className="absolute top-20 left-0"
             variants={boxes(1.2)}
           >
             <img
@@ -40,16 +37,18 @@ const HeroPage = () => (
               className="w-[120px]  object-contain"
             />
           </motion.div>
-          <motion.div className='absolute bottom-5 left-10'
-            variants={boxes(1.3)}>
+          <motion.div className="absolute bottom-5 left-10"
+            variants={boxes(1.3)}
+          >
             <img
               src="/react.png"
               alt="get-started"
               className="w-[100px]  object-contain"
             />
           </motion.div>
-          <motion.div className='absolute bottom-40 right-0'
-            variants={boxes(1.4)}>
+          <motion.div className="absolute bottom-40 right-0"
+            variants={boxes(1.4)}
+          >
             <img
               src="/tailwind.png"
               alt="get-started"
@@ -57,10 +56,7 @@ const HeroPage = () => (
             />
           </motion.div>
 
-
         </div>
-
-
 
       </motion.div>
 
@@ -80,11 +76,11 @@ const HeroPage = () => (
           ))} */}
           <p className="my-[16px] font-normal lg:text-[20px] text-[14px] text-secondary-white">Even though my name is rizky, it’s totally safe to do business with me. I create and design website for small business. I can provide clean code and pixel perfect responsive design.
           </p>
-            <motion.a
-              whileHover={{ scale: 1.1 }}
-              href="#contact" class="py-3 px-5 text-sm font-medium text-center text-black rounded-lg bg-orange items-center justify-center w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" >Contact Me
-            </motion.a>
-
+          <motion.a
+            whileHover={{ scale: 1.1 }}
+            href="#contact" class="py-3 px-5 text-sm font-medium text-center text-black rounded-lg bg-orange items-center justify-center w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >Contact Me
+          </motion.a>
 
         </div>
       </motion.div>
