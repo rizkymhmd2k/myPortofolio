1f08d5fb0baa6e514c4e48e8f5bba5fa26866309
Muhammad Rizky <muhammadrizky@rizkym1.local>
2023-05-02 19:50:09 +0800

    fav3

--- app/head.js
+++ app/head.js
@@ -2,13 +2,8 @@ const Head = () => (
   <>
     <title>Metaversus</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-<<<<<<< HEAD
     <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-=======
-    {/* <link rel="icon" href="./favicon.ico?" /> */}
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico?" />
 
->>>>>>> a8665ff (fav1)
   </>
 );
 
