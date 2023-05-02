c6a904f6a50aea534bec90038558f5abff363faf
Muhammad Rizky <muhammadrizky@rizkym1.local>
2023-05-02 21:33:20 +0800

    fav7

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ const MyApp = ({ Component, pageProps }) => (
     <Head>
       <title>Rizky</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.png" />
+      <link rel="icon" href="/images/favicon.png" />
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
     </Head>
